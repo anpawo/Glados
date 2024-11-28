0f7c689b206f13d7a62655c5a0039fa605dd4ce6
Marius Rousset <marius.rousset@epitech.eu>
2024-11-28 17:48:58 +0100

    feat: function definition and variable defition done. and some tests.

--- src/Lisp/Ast.hs
+++ src/Lisp/Ast.hs
@@ -4,7 +4,6 @@
 -- File description:
 -- AST
 -}
-{-# LANGUAGE InstanceSigs #-}
 {-# OPTIONS_GHC -Wno-incomplete-uni-patterns #-}
 {-# HLINT ignore "Use newtype instead of data" #-}
 {-# OPTIONS_GHC -Wno-partial-fields #-}
@@ -16,21 +15,22 @@ module Lisp.Ast
   )
 where
 
-import Lisp.SExpression (SExpr (..))
+import Lisp.ErrorMessage
+import Lisp.SExpression (SExpr (..), getSymbol)
+
+-- TODO: The Body is a single expression, we don't handle many expression
 
 data Ast
   = TInt Int
   | TSymbol String
   | TString String
   | TBool Bool
-  | TVoid
-  | -- above is 100% sure
-    TVariable {varName :: String, varBody :: Ast} -- TODO: can be a value or a lambda
-  | TDefine {fnName :: String, fnArgs :: [String], defFnBody :: [Ast]} -- TODO: body should be anything except another define
+  | TVariable {varName :: String, varBody :: Ast}
+  | TFunction {fnName :: String, fnArgs :: [String], defFnBody :: Ast}
   | TCall {fnName :: String, callFnBody :: Ast} -- TODO
   | TLambda -- TODO
-  | -- should exist ?
-    TList [Ast]
+  -- TVoid
+  -- TList [Ast]
   deriving (Show)
 
 type AstError = String
@@ -41,10 +41,10 @@ instance Eq Ast where
   TSymbol x == TSymbol y = x == y
   TString x == TString y = x == y
   TBool x == TBool y = x == y
-  TVoid == TVoid = True
-  TList x == TList y = x == y
+  -- TVoid == TVoid = True
+  -- TList x == TList y = x == y
   TVariable name1 body1 == TVariable name2 body2 = name1 == name2 && body1 == body2
-  TDefine fnName1 fnArgs1 fnBody1 == TDefine fnName2 fnArgs2 fnBody2 = fnName1 == fnName2 && fnArgs1 == fnArgs2 && fnBody1 == fnBody2
+  TFunction fnName1 fnArgs1 fnBody1 == TFunction fnName2 fnArgs2 fnBody2 = fnName1 == fnName2 && fnArgs1 == fnArgs2 && fnBody1 == fnBody2
   TCall fnName1 body1 == TCall fnName2 body2 = fnName1 == fnName2 && body1 == body2
   TLambda == TLambda = True
   _ == _ = False
@@ -64,35 +64,40 @@ handleList functionNameAndArgs = handleCall functionNameAndArgs
 handleCall :: [SExpr] -> Either AstError Ast
 handleCall _ = Left "todo"
 
-defineVariable :: String -> [SExpr] -> Either AstError Ast
-defineVariable name [value] =
+defineVariable :: String -> SExpr -> Either AstError Ast
+defineVariable name value =
   case sexprToAST value of
-    Right x -> Right (TVariable name x)
+    Right x | isDefine x -> Left $ errVarDef $ "cannot contain another define" ++ show x
+    Right x -> Right $ TVariable name x
     Left err -> Left err
-defineVariable _ _ = Left "defineVariable: not implemented yet (lambda)"
 
-defineFunction :: [SExpr] -> [SExpr] -> Either AstError Ast
-defineFunction args _ | not (all isSymbol args) = Left $ "Invalid function definition: variable's name must be Symbols." ++ show args
-defineFunction args _ | any isKeyword args = Left $ "Invalid function definition: variable's name mustn't be Keywords." ++ show args
-defineFunction ((SSymbol name) : args) body =
-  case mapM sexprToAST body of
-    Right l -> Right $ TDefine name (map (\(SSymbol s) -> s) args) l
-    Left err -> Left err
-defineFunction _ _ = Left "Invalid function definition: missing function name."
+-- The body function cannot be [TVoid, TDefine, TVariable]
+-- tho, it should never be able to contain a void since void can only be affected to symbols
+defineFunction :: [SExpr] -> SExpr -> Either AstError Ast
+defineFunction argsSExpr body =
+  case mapM getSymbol argsSExpr of
+    Just allArgs@(name : args)
+      | not (any (`elem` keywords) allArgs) -> case sexprToAST body of
+          Right x | isDefine x -> Left $ errFnDef $ "the body cannot contain a define:" ++ show x
+          Right validBody -> Right $ TFunction name args validBody
+          Left err -> Left $ errFnDef $ show err
+      | otherwise -> Left $ errFnDef "variable's name cannot be Keywords"
+    Just [] -> Left $ errFnDef "missing function name."
+    Nothing -> Left $ errFnDef $ "variables' name must be Symbols." ++ show body
 
 handleDefine :: [SExpr] -> Either AstError Ast
-handleDefine [] = Left "Invalid define: missing function/variable name."
-handleDefine (SInt x : _) = Left $ "Invalid define: cannot have a name starting with a number: " ++ show x
-handleDefine (SString x : _) = Left $ "Invalid define: cannot have a string as name: " ++ show x
-handleDefine (SSymbol name : value) = defineVariable name value
-handleDefine (SList args : body) = defineFunction args body
+handleDefine [] = Left $ errDef "missing args and body."
+handleDefine [_] = Left $ errDef "missing body."
+handleDefine (SInt x : _) = Left $ errDef $ "args must be Symbols: " ++ show x
+handleDefine (SString x : _) = Left $ errDef $ "args must be Symbols: " ++ show x
+handleDefine [SList args, body] = defineFunction args body
+handleDefine [SSymbol name, value] = defineVariable name value
+handleDefine (_ : l : r) = Left $ errDef $ "the body must contain only one expression: " ++ show l ++ " " ++ show r
 
-isSymbol :: SExpr -> Bool
-isSymbol (SSymbol _) = True
-isSymbol _ = False
+keywords :: [String]
+keywords = ["define", "lambda"]
 
-isKeyword :: SExpr -> Bool
-isKeyword (SSymbol "define") = True
-isKeyword (SSymbol "lambda") = True
-isKeyword (SSymbol "set!") = True
-isKeyword _ = False
+isDefine :: Ast -> Bool
+isDefine (TFunction {}) = True
+isDefine (TVariable {}) = True
+isDefine _ = False
