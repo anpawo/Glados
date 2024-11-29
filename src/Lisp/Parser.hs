918157eb4d22179f6ab99b6d555984e04a2edcc1
Marius Rousset <marius.rousset@epitech.eu>
2024-11-29 16:34:31 +0100

    feat: start of parser

--- src/Lisp/Parser.hs
+++ src/Lisp/Parser.hs
@@ -5,11 +5,75 @@
 -- Parser
 -}
 
-module Lisp.Parser () where
+module Lisp.Parser
+  ( Parsec,
+    choice,
+    many,
+    parse,
+    parseTest,
+    some,
+    try,
+    SExpr (..),
+    Text,
+    pack,
+    Void,
+    char,
+    newline,
+    string,
+    decimal,
+    hexadecimal,
+    signed,
+    symbol,
+    t,
+    parseSInt,
+    letterChar,
+    parseSSymbol,
+    between,
+    anySingle,
+    parseSString,
+  )
+where
 
-import Text.Megaparsec ()
-import Text.Megaparsec.Char ()
-import Data.Text (Text)
+import Data.Text (Text, pack)
 import Data.Void (Void)
+import Lisp.SExpression (SExpr (..))
+import Text.Megaparsec (Parsec, anySingle, between, choice, many, noneOf, parse, parseTest, some, try)
+import Text.Megaparsec.Char (alphaNumChar, char, letterChar, newline, string)
+import Text.Megaparsec.Char.Lexer (decimal, hexadecimal, signed, symbol)
+
+-- import Text.Megaparsec.Combinator (some)
 
 type Parser = Parsec Void Text
+
+t :: Parser SExpr -> String -> IO ()
+t p s = parseTest p (pack s)
+
+parseSInt :: Parser SExpr
+parseSInt = SInt <$> signed mempty decimal
+
+parseSSymbol :: Parser SExpr
+parseSSymbol = SSymbol <$> ((:) <$> letterChar <*> many alphaNumChar)
+
+parseSString :: Parser SExpr
+parseSString = SString <$> (char '"' *> many (noneOf "\"") <* char '"')
+
+parseSList :: Parser SExpr
+parseSList = SList <$> p
+
+parseInsideList :: Parser SExpr
+parseInsideList = choice
+    [ 
+    ]
+
+-- parseOneExpression :: Parser SExpr
+--     parseSpace
+--     (parseInt -> parseInput)
+--     (parseSymbol -> parseInput)
+--     (parseString -> parseInput)
+--     (parseList -> parseInput)
+--     parseEof
+
+-- parseInput :: Parser SExpr
+-- parseInput = parseOneExpression
+
+-- parseComments :: Parser Void
