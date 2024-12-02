efa6e255a8aa488b87bf23f2ca8c7694c4c531bb
Marius Rousset <marius.rousset@epitech.eu>
2024-12-02 17:51:20 +0100

    feat: virtual machine

--- src/Lisp/VirtualMachine.hs
+++ src/Lisp/VirtualMachine.hs
@@ -4,35 +4,51 @@
 -- File description:
 -- Virtual Machine
 -}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
 
-{-# HLINT ignore "Use print" #-}
-
-module Lisp.VirtualMachine (runFile, interpreter) where
+module Lisp.VirtualMachine (interpreter) where
 
 import Control.Exception (catch)
 import Control.Exception.Base (IOException)
 import Data.Text (pack)
 import Lisp.Ast (Ast)
 import Lisp.Parser (parseInput, runParser)
+import System.Exit (ExitCode (ExitFailure), exitWith)
 import System.IO (hFlush, stdout)
 import Text.Megaparsec (errorBundlePretty)
 
-runFile :: String -> IO ()
-runFile = putStrLn
-
 -- parseSExpr -> Ast -> Execute -> Return and display result except void
-interpreter :: [Ast] -> IO ()
-interpreter ctx = catch runInterpreter eofHandler
+interpreter :: [Ast] -> String -> IO ()
+interpreter ctx lastInput = catch runInterpreter eofHandler
   where
-    runInterpreter = printPrompt >> getLine >>= handleInput >> interpreter ctx
+    runInterpreter = printPrompt lastInput >> getLine >>= handleInput ctx lastInput
 
-    printPrompt = putStr "> " >> hFlush stdout
-
-    handleInput "" = return ()
-    handleInput input = case runParser parseInput "" (pack input) of
-      Left err -> putStr (errorBundlePretty err)
-      Right expr -> putStrLn $ show expr
+    printPrompt "" = putStr "glados> " >> hFlush stdout
+    printPrompt _ = putStr "......> " >> hFlush stdout
 
     eofHandler :: IOException -> IO ()
     eofHandler _ = return ()
+
+handleInput :: [Ast] -> String -> String -> IO ()
+handleInput ctx lastInput "" = interpreter ctx lastInput
+handleInput ctx lastInput currInput
+  | isWaitingEnd totalInput = interpreter ctx totalInput
+  | otherwise = executeExpr ctx totalInput
+  where
+    totalInput = lastInput ++ currInput
+
+isWaitingEnd :: String -> Bool
+isWaitingEnd input = sameNumberParent input /= 0
+  where
+    sameNumberParent :: String -> Int
+    sameNumberParent "" = 0
+    sameNumberParent ('(' : s) = 1 + sameNumberParent s
+    sameNumberParent (')' : s) = (-1) + sameNumberParent s
+    sameNumberParent ('"' : s) = case dropWhile (/= '"') s of
+      "" -> 0
+      (_ : rst) -> sameNumberParent rst
+    sameNumberParent (_ : s) = 0 + sameNumberParent s
+
+executeExpr :: [Ast] -> String -> IO ()
+executeExpr ctx input = case runParser parseInput "" (pack input) of
+  Left err -> putStr (errorBundlePretty err) >> exitWith (ExitFailure 84)
+  Right expr -> putStrLn ("expr: " ++ show expr) >> interpreter ctx ""
