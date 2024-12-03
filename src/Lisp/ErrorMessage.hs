d47b390852484616723398a7b8a92e433f24276e
Marius Rousset <marius.rousset@epitech.eu>
2024-12-03 18:26:02 +0100

    feat: add some new error message for better clarity

--- src/Lisp/ErrorMessage.hs
+++ src/Lisp/ErrorMessage.hs
@@ -5,10 +5,22 @@
 -- ErrorMessage
 -}
 
-module Lisp.ErrorMessage (errDef, errFnDef, errVarDef) where
+module Lisp.ErrorMessage (errDef, errFnDef, errVarDef, errCall, errDefCtx, errIf, errLambda) where
 
-errDef :: String -> String
-errDef err = "Error: Invalid define: " ++ err
+errIf :: String -> String
+errIf err = "Error: Invalid if: " ++ err
+
+errLambda :: String -> String
+errLambda err = "Error: Invalid lambda: " ++ err
+
+errDefCtx :: String
+errDefCtx = "Error: Invalid context for a define"
+
+errCall :: String -> String
+errCall err = "Error: Invalid call: " ++ err
+
+errDef :: String
+errDef = "Error: Invalid define"
 
 errFnDef :: String -> String
 errFnDef err = "Error: Invalid function definition: " ++ err
