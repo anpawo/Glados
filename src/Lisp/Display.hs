28aec93c2ba0f6eb9ccacc2e30a25460b866e0ff
Marius Rousset <marius.rousset@epitech.eu>
2024-12-04 23:36:02 +0100

    feat: display of Ast single expression

---
+++ src/Lisp/Display.hs
@@ -0,0 +1,23 @@
+{-
+-- EPITECH PROJECT, 2024
+-- glados
+-- File description:
+-- Display
+-}
+
+module Lisp.Display (astToString) where
+
+import Lisp.Ast (Ast (..), Ctx)
+
+procedure :: [String] -> String
+procedure [name] = "#<procedure " ++ name ++ ">"
+procedure _ = "#<procedure>"
+
+astToString :: Ctx -> Ast -> String
+astToString _ TVoid = "void"
+astToString _ (TBool True) = "#t"
+astToString _ (TBool False) = "#f"
+astToString _ (TInt x) = show x
+astToString _ (TString x) = show x
+astToString _ (TLambda name _) = procedure name -- Lambda can be without any arguments
+astToString _ _ = ""
