f197850740fb19cf570b2e06ca3b818329a73e20
Marius Rousset <marius.rousset@epitech.eu>
2024-12-05 05:08:23 +0100

    feat: function evaluation and if

--- src/Lisp/Evaluate.hs
+++ src/Lisp/Evaluate.hs
@@ -7,35 +7,27 @@
 
 module Lisp.Evaluate (evalAst) where
 
-import Data.List (find)
+import Data.List (elemIndex, find, nub)
 import Lisp.Ast (Ast (..), Ctx, trueIfTruthy)
+import Lisp.ErrorMessage
 
 type EvalErr = String
 
-unboundVar :: String -> String
-unboundVar name = "Error: variable " ++ name ++ " is not bound"
-
-impossibleCase :: String
-impossibleCase = "Impossible: This should never happend."
+type Args = [Ast]
 
 evalAst :: Ctx -> Ast -> Either EvalErr (Ast, Ctx)
-evalAst _ (TFunction {}) = Left impossibleCase -- only part of the context
-evalAst _ (TVariable {}) = Left impossibleCase -- only part of the context
+evalAst _ (TFunction {}) = Left errImpossible -- only part of the context
+evalAst _ (TVariable {}) = Left errImpossible -- only part of the context
 evalAst ctx x@(TInt {}) = Right (x, ctx)
 evalAst ctx x@(TBool {}) = Right (x, ctx)
 evalAst ctx x@TVoid = Right (x, ctx)
 evalAst ctx x@(TString {}) = Right (x, ctx)
 evalAst ctx TLambda {} = Right (TLambda [] TVoid, ctx) -- TODO: compute the lambda
-evalAst ctx (TVariableCall name) = (,) <$> (retrieveVariable ctx name >>= Right) <*> Right ctx
 evalAst ctx (TDefineVariable name body) = (,) <$> Right TVoid <*> defineVariable ctx name body
 evalAst ctx (TDefineFunction name body) = (,) <$> Right TVoid <*> defineFunction ctx name body
-evalAst ctx (TIf c t e) = (,) <$> ifEval ctx c t e <*> Right ctx
--- evalAst _ (TFunctionCall {}) = print "" >> return (Left "todo")
-evalAst _ _ = Left "todo: evalAst"
-
-eval' :: Ctx -> Ast -> Either EvalErr (Ast, Ctx)
-eval' ctx x@(TLambda {}) = (,) <$> functionEval ctx x <*> Right ctx
-eval' ctx x = evalAst ctx x
+evalAst ctx (TIf c t e) = (,) <$> ifEval ctx c t e <*> Right ctx -- the error might be there
+evalAst ctx (TVariableCall name) = (,) <$> (retrieveVariable ctx name >>= Right) <*> Right ctx
+evalAst ctx (TFunctionCall name args) = (,) <$> functionEval ctx name args <*> Right ctx
 
 ifEval :: Ctx -> Ast -> Ast -> Ast -> Either EvalErr Ast
 ifEval ctx c t e =
@@ -44,19 +36,13 @@ ifEval ctx c t e =
     Right _ -> evalAst ctx e >>= Right . fst
     Left err -> Left err
 
--- \| trueIfTruthy condition = ""
--- \| otherwise = ""
-
-functionEval :: Ctx -> Ast -> Either EvalErr Ast
-functionEval _ _ = Left "todo: functionEval"
-
 retrieveVariable :: Ctx -> String -> Either EvalErr Ast
 retrieveVariable ctx name =
   case find (sameName name) ctx of
-    Nothing -> Left $ unboundVar name
+    Nothing -> Left $ errUnboundVar name
     Just (TVariable _ val) -> Right val
-    Just (TFunction name' _) -> Right (TLambda [name'] TVoid)
-    _ -> Left impossibleCase -- impossible
+    Just (TFunction name' _) -> Right (TLambda [name'] TVoid) -- handles the procedure message but is a little trick. may be removed.
+    _ -> Left errImpossible
 
 sameName :: String -> Ast -> Bool
 sameName name (TVariable name' _) = name == name'
@@ -64,7 +50,7 @@ sameName name (TFunction name' _) = name == name'
 sameName _ _ = False
 
 defineVariable :: Ctx -> String -> Ast -> Either EvalErr Ctx
-defineVariable ctx name body = (:) <$> (eval' newCtx body >>= (Right . TVariable name . fst)) <*> Right newCtx
+defineVariable ctx name body = (:) <$> (evalAst newCtx body >>= (Right . TVariable name . fst)) <*> Right newCtx
   where
     newCtx = filter (not . sameName name) ctx
 
@@ -72,3 +58,99 @@ defineFunction :: Ctx -> String -> Ast -> Either EvalErr Ctx
 defineFunction ctx name body = (:) <$> Right (TFunction name body) <*> Right newCtx
   where
     newCtx = filter (not . sameName name) ctx
+
+lambdaEval :: Ctx -> Args -> Ast -> Either EvalErr Ast
+lambdaEval _ eargs (TLambda args _) | length eargs /= length args = Left $ errNumberArgs "lambda"
+lambdaEval ctx eargs (TLambda args body) = combineContext ctx eargs args body >>= (`evalAst` body) >>= (Right . fst)
+lambdaEval _ _ _ = Left errImpossible
+
+combineContext :: Ctx -> Args -> [String] -> Ast -> Either EvalErr Ctx
+combineContext ctx eargs args ast = updateCtxWSymbol ctx eargs args symbols
+  where
+    symbols = nub $ retrieveAllSymbols ast
+
+updateCtxWSymbol :: Ctx -> Args -> [String] -> [String] -> Either EvalErr Ctx
+updateCtxWSymbol ctx _ _ [] = Right ctx
+updateCtxWSymbol ctx eargs args (s : symbols) = case elemIndex s args of
+  Nothing -> updateCtxWSymbol ctx eargs args symbols
+  Just index -> case eargs !! index of
+    x@(TLambda {}) -> updateCtxWSymbol (TFunction s x : newCtx) eargs args symbols
+    x -> case evalAst ctx x of
+      Left err -> Left err
+      Right (v, _) -> updateCtxWSymbol (TVariable s v : newCtx) eargs args symbols
+    where
+      newCtx = filter (not . sameName s) ctx
+
+retrieveAllSymbols :: Ast -> [String]
+retrieveAllSymbols (TVariableCall name) = [name]
+retrieveAllSymbols (TFunctionCall name body) = name : concatMap retrieveAllSymbols body
+retrieveAllSymbols (TIf c t e) = retrieveAllSymbols c ++ retrieveAllSymbols t ++ retrieveAllSymbols e
+retrieveAllSymbols _ = []
+
+functionEval :: Ctx -> String -> Args -> Either EvalErr Ast
+functionEval ctx "eq?" args = builtinEq ctx args
+functionEval ctx "<" args = builtinLT ctx args
+functionEval ctx "+" args = builtinAdd ctx args
+functionEval ctx "-" args = builtinSub ctx args
+functionEval ctx "*" args = builtinMul ctx args
+functionEval ctx "div" args = builtinDiv ctx args
+functionEval ctx "mod" args = builtinMod ctx args
+functionEval ctx name args =
+  case find (sameName name) ctx of
+    Nothing -> Left $ errUnboundVar name
+    Just (TVariable {}) -> Left $ errNonProcedure name
+    Just (TFunction _ lambda) -> lambdaEval ctx args lambda
+    _ -> Left errImpossible
+
+builtinEq :: Ctx -> Args -> Either EvalErr Ast
+builtinEq ctx [l, r] = ((,) <$> (evalAst ctx l >>= Right . fst) <*> (evalAst ctx r >>= Right . fst)) >>= evalBuiltin
+  where
+    -- evalBuiltin (a, b) = Left $ show a ++ ", " ++ show b
+    evalBuiltin (a, b) = Right $ TBool (a == b)
+builtinEq _ _ = Left $ errNumberArgs "eq?"
+
+builtinLT :: Ctx -> Args -> Either EvalErr Ast
+builtinLT ctx [l, r] = ((,) <$> (evalAst ctx l >>= Right . fst) <*> (evalAst ctx r >>= Right . fst)) >>= evalBuiltin
+  where
+    evalBuiltin (TInt a, TInt b) = Right $ TBool (a < b)
+    evalBuiltin _ = Left $ errTypeArgs "<" "bool"
+builtinLT _ _ = Left $ errNumberArgs "<"
+
+builtinAdd :: Ctx -> Args -> Either EvalErr Ast
+builtinAdd ctx [l, r] = ((,) <$> (evalAst ctx l >>= Right . fst) <*> (evalAst ctx r >>= Right . fst)) >>= evalBuiltin
+  where
+    evalBuiltin (TInt a, TInt b) = Right $ TInt (a + b)
+    evalBuiltin _ = Left $ errTypeArgs "+" "int"
+builtinAdd _ _ = Left $ errNumberArgs "+"
+
+builtinSub :: Ctx -> Args -> Either EvalErr Ast
+builtinSub ctx [l, r] = ((,) <$> (evalAst ctx l >>= Right . fst) <*> (evalAst ctx r >>= Right . fst)) >>= evalBuiltin
+  where
+    -- evalBuiltin (a, b) = Left $ show a ++ ", " ++ show b
+    evalBuiltin (TInt a, TInt b) = Right $ TInt (a - b)
+    evalBuiltin _ = Left $ errTypeArgs "-" "int"
+builtinSub _ _ = Left $ errNumberArgs "-"
+
+builtinMul :: Ctx -> Args -> Either EvalErr Ast
+builtinMul ctx [l, r] = ((,) <$> (evalAst ctx l >>= Right . fst) <*> (evalAst ctx r >>= Right . fst)) >>= evalBuiltin
+  where
+    -- evalBuiltin (a, b) = Left $ show a ++ ", " ++ show b
+    evalBuiltin (TInt a, TInt b) = Right $ TInt (a * b)
+    evalBuiltin _ = Left $ errTypeArgs "*" "int"
+builtinMul _ _ = Left $ errNumberArgs "*"
+
+builtinDiv :: Ctx -> Args -> Either EvalErr Ast
+builtinDiv ctx [l, r] = ((,) <$> (evalAst ctx l >>= Right . fst) <*> (evalAst ctx r >>= Right . fst)) >>= evalBuiltin
+  where
+    evalBuiltin (TInt _, TInt 0) = Left "Error: Division by 0"
+    evalBuiltin (TInt a, TInt b) = Right $ TInt (a `div` b)
+    evalBuiltin _ = Left $ errTypeArgs "div" "int"
+builtinDiv _ _ = Left $ errNumberArgs "div"
+
+builtinMod :: Ctx -> Args -> Either EvalErr Ast
+builtinMod ctx [l, r] = ((,) <$> (evalAst ctx l >>= Right . fst) <*> (evalAst ctx r >>= Right . fst)) >>= evalBuiltin
+  where
+    evalBuiltin (TInt _, TInt 0) = Left "Error: Division by 0"
+    evalBuiltin (TInt a, TInt b) = Right $ TInt (a `mod` b)
+    evalBuiltin _ = Left $ errTypeArgs "mod" "int"
+builtinMod _ _ = Left $ errNumberArgs "mod"
