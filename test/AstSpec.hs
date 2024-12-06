a5710368b7472ccbda9bd9041a79edb815a08a2e
Marius Rousset <marius.rousset@epitech.eu>
2024-12-06 16:26:38 +0100

    fix: unit-tests with the ast changes

---
+++ test/AstSpec.hs
@@ -0,0 +1,26 @@
+--
+-- EPITECH PROJECT, 2024
+-- glados
+-- File description:
+-- TestTemplateSpec
+--
+
+module AstSpec (spec) where
+
+import Lisp.Ast (Ast (..), sexprToAST)
+import Lisp.SExpression (SExpr (..))
+import Test.Hspec (Spec, describe, it, shouldBe)
+
+spec :: Spec
+spec = do
+  describe "simple variable definition" $ do
+    it "int into variable" $
+      sexprToAST (SList [SSymbol "define", SSymbol "y", SInt 5]) `shouldBe` Right (TVariable "y" (TInt 5))
+    it "string into variable" $
+      sexprToAST (SList [SSymbol "define", SSymbol "y", SString "Hello there"]) `shouldBe` Right (TVariable "y" (TString "Hello there"))
+
+  describe "simple function definition" $ do
+    it "no argument and default int value" $
+      sexprToAST (SList [SSymbol "define", SList [SSymbol "int5"], SInt 5]) `shouldBe` Right (TFunction "int5" (TLambda [] (TInt 5)))
+    it "one argument and default int value" $
+      sexprToAST (SList [SSymbol "define", SList [SSymbol "int5", SSymbol "unusedVar"], SInt 5]) `shouldBe` Right (TFunction "int5" (TLambda ["unusedVar"] (TInt 5)))
