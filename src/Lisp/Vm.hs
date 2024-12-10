18ebb3095047f514163bc809a8cd6aba075ec0fa
QuentinStoll <quentin.stoll@epitech.eu>
2024-12-10 20:19:26 +0100

    feat (virtual machine): fourth step vm

--- src/Lisp/Vm.hs
+++ src/Lisp/Vm.hs
@@ -20,16 +20,19 @@ data Inst = Push Value
           | Call Op
           | Ret
           | JumpIfFalse Int
+          | PushArg Int
   deriving (Show, Eq)
 
 -- Aliases
 type Stack = [Value]
 type Insts = [Inst]
+type Args = [Value]
 
-exec :: Insts -> Stack -> Either String Value
-exec [] stack = Left "Error: No ret instruction ?"
-exec (Push v : rest) stack = exec rest (v : stack)
-exec (Call op : rest) (v1 : v2 : stack) = 
+-- Fonction d'exécution
+exec :: Args -> Insts -> Stack -> Either String Value
+exec _ [] stack = Left "Error: No ret instruction ?"
+exec args (Push v : rest) stack = exec args rest (v : stack)
+exec args (Call op : rest) (v1 : v2 : stack) = 
   let result = case (op, v1, v2) of
         (Add, IntVal a, IntVal b) -> Right (IntVal (a + b))
         (Sub, IntVal a, IntVal b) -> Right (IntVal (a - b))
@@ -41,50 +44,57 @@ exec (Call op : rest) (v1 : v2 : stack) =
         _ -> Left "Error: You cant do that"
   in case result of
        Left err -> Left err
-       Right res -> exec rest (res : stack)
-exec (JumpIfFalse n : rest) (BoolVal False : stack) = 
-  let skip = take n rest
-  in exec (drop n rest) stack
-exec (JumpIfFalse _ : rest) (_ : stack) = exec rest stack
-exec (Ret : _) (v : _) = Right v
-exec _ _ = Left "Error: Invalid program or stack state"
+       Right res -> exec args rest (res : stack)
+exec args (JumpIfFalse n : rest) (BoolVal False : stack) = 
+  exec args (drop n rest) stack
+exec args (JumpIfFalse _ : rest) (_ : stack) = exec args rest stack
+exec args (PushArg i : rest) stack =
+  if i < 0 || i >= length args
+  then Left $ "Error: Invalid argument index " ++ show i
+  else exec args rest ((args !! i) : stack)
+exec _ (Ret : _) (v : _) = Right v
+exec _ _ _ = Left "Error: Invalid program or stack state"
 
--- TEST VM
+-- Tests func
 test_vm :: IO ()
 test_vm = do
-  --TESTS UNITAITES PAR LA SUITE
-  -- Exemple 1 : Retourne 42
+  -- TESTS UNIT A FAIRE
+  -- Test 1 : Simple retour d'une valeur
   let program1 = [Push (IntVal 42), Ret]
-  print $ exec program1 []
+  print $ exec [] program1 []  -- Résultat attendu : Right (IntVal 42)
 
-  -- Exemple 2 : Soustraction de 52 et 10 (devrait donner 42)
+  -- Test 2 : Opérations arithmétiques (52 - 10)
   let program2 = [Push (IntVal 10), Push (IntVal 52), Call Sub, Ret]
-  print $ exec program2 []
+  print $ exec [] program2 []  -- Résultat attendu : Right (IntVal 42)
 
-  -- Exemple 3 : Division par zéro (devrait donner une erreur)
+  -- Test 3 : Division par zéro
   let program3 = [Push (IntVal 0), Push (IntVal 42), Call Div, Ret]
-  print $ exec program3 []
+  print $ exec [] program3 []  -- Résultat attendu : Left "Error: You cant divide by 0"
 
-  -- Exemple 4 : Tentative d'addition entre un Int et un Bool (devrait donner une erreur)
+  -- Test 4 : Opération invalide (Int + Bool)
   let program4 = [Push (IntVal 10), Push (BoolVal True), Call Add, Ret]
-  print $ exec program4 []
+  print $ exec [] program4 []  -- Résultat attendu : Left "Error: You cant do that"
 
-  -- Exemple 5 : Utilisation de `Eq` pour tester l'égalité
+  -- Test 5 : Comparaison d'égalité
   let program5 = [Push (IntVal 10), Push (IntVal 10), Call Eq, Ret]
-  print $ exec program5 []  -- Doit retourner True
+  print $ exec [] program5 []  -- Résultat attendu : Right (BoolVal True)
 
-  -- Exemple 6 : Utilisation de `Less` pour tester la comparaison
+  -- Test 6 : Comparaison "moins que"
   let program6 = [Push (IntVal 10), Push (IntVal 5), Call Less, Ret]
-  print $ exec program6 []  -- Doit retourner True
+  print $ exec [] program6 []  -- Résultat attendu : Right (BoolVal True)
 
-  -- Exemple 6B : Utilisation de `Less` pour tester la comparaison (SUJET)
-  let program6 = [Push (IntVal 2), Push (IntVal 5), Call Less, Ret]
-  print $ exec program6 []  -- Doit retourner False
+  -- Test 7 : Saut conditionnel (ne saute pas)
+  let program7 = [Push (BoolVal True), JumpIfFalse 2, Push (IntVal 99), Ret]
+  print $ exec [] program7 []  -- Résultat attendu : Right (IntVal 99)
 
-  -- Exemple 7 : Utilisation de `JumpIfFalse` pour sauter une instruction
-  let program7 = [Push (BoolVal False), JumpIfFalse 2, Push (IntVal 99), Ret]
-  print $ exec program7 []  -- Doit sauter la valeur 99 et retourner l'erreur d'absence de `Ret`
+  -- Test 8 : Saut conditionnel (saute)
+  let program8 = [Push (BoolVal False), JumpIfFalse 2, Push (IntVal 99), Ret]
+  print $ exec [] program8 []  -- Résultat attendu : Left "Error: No ret instruction ?"
 
-  -- Exemple 8 : `JumpIfFalse` où la condition est vraie, ne saute pas
-  let program8 = [Push (BoolVal True), JumpIfFalse 2, Push (IntVal 99), Ret]
-  print $ exec program8 []  -- Doit retourner 99
+  -- Test 9 : Utilisation de `PushArg`
+  let program9 = [PushArg 0, Push (IntVal 2), Call Mul, Ret]
+  print $ exec [IntVal 21] program9 []  -- Résultat attendu : Right (IntVal 42)
+
+  -- Test 10 : Accès invalide à un argument
+  let program10 = [PushArg 1, Ret]
+  print $ exec [IntVal 42] program10 []  -- Résultat attendu : Left "Error: Invalid argument index 1"
