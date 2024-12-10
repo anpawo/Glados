b6ab14119cf1b45b5b8ad8a0efc1a17f33ec3b49
AzizEnnaifer <aziz.ennaifer@epitech.eu>
2024-12-10 04:38:46 +0100

    add: Interpreter unit testing draft

--- test/Spec.hs
+++ test/Spec.hs
@@ -8,11 +8,13 @@
 -- import AstSpec (spec)
 import SExprParserSpec (spec)
 import SExpressionSpec (spec)
+--import InterpreterSpec (spec)
 import Test.Hspec (describe, hspec)
 
 main :: IO ()
 main = hspec $ do
   describe "Parsing String to SExpr" SExprParserSpec.spec
   describe "SExpression getter" SExpressionSpec.spec
+  --describe "Interpreter tests" InterpreterSpec.spec
 
 --   -- describe "Ast" AstSpec.spec
