8c6375896c964b73065aa0fb7231cf81a4e93e15
QuentinStoll <quentin.stoll@epitech.eu>
2024-12-10 19:36:04 +0100

    feat (virtual machine): second step vm

--- src/Lisp/Vm.hs
+++ src/Lisp/Vm.hs
@@ -7,44 +7,56 @@
 
 module Lisp.Vm (test_vm) where
 
--- Def base types
+-- Base types
 data Value = IntVal Int | BoolVal Bool
   deriving (Show, Eq)
 
--- Def oper
+-- Oper
 data Op = Add | Sub | Mul | Div
   deriving (Show, Eq)
 
--- Def instructions
-data Inst = Push Value       -- Here you can add a value on the stack
-          | Call Op          -- Here you can add an operator on the stack
-          | Ret              -- Return the value at the top of the stack
+-- Insts
+data Inst = Push Value
+          | Call Op
+          | Ret
   deriving (Show, Eq)
 
--- Def aliases
+-- Aliases
 type Stack = [Value]
 type Insts = [Inst]
 
--- Func to execute the stack
-exec :: Insts -> Stack -> Value
-exec [] stack = error "Program terminated without Ret instruction"
+exec :: Insts -> Stack -> Either String Value
+exec [] stack = Left "Error: No ret instruction ?"
 exec (Push v : rest) stack = exec rest (v : stack)
 exec (Call op : rest) (v1 : v2 : stack) = 
   let result = case (op, v1, v2) of
-        (Add, IntVal a, IntVal b) -> IntVal (a + b)
-        (Sub, IntVal a, IntVal b) -> IntVal (b - a)
-        (Mul, IntVal a, IntVal b) -> IntVal (a * b)
-        (Div, IntVal a, IntVal b) -> if a == 0 then error "Division by zero" else IntVal (b `div` a)
-        _ -> error "Invalid operation or operands"
-  in exec rest (result : stack)
-exec (Ret : _) (v : _) = v
-exec _ _ = error "Invalid program or stack state"
+        (Add, IntVal a, IntVal b) -> Right (IntVal (a + b))
+        (Sub, IntVal a, IntVal b) -> Right (IntVal (a - b))
+        (Mul, IntVal a, IntVal b) -> Right (IntVal (a * b))
+        (Div, IntVal _ , IntVal 0) -> Left "Error: You cant divide by 0"
+        (Div, IntVal a, IntVal b) -> Right (IntVal (a `div` b))
+        _ -> Left "Error: You cant do that"
+  in case result of
+       Left err -> Left err
+       Right res -> exec rest (res : stack)
+exec (Ret : _) (v : _) = Right v
+exec _ _ = Left "Error: Invalid program or stack state"
 
 -- TEST VM
 test_vm :: IO ()
 test_vm = do
+  -- Expl 1:
   let program1 = [Push (IntVal 42), Ret]
   print $ exec program1 []
-  let program2 = [Push (IntVal 52), Push (IntVal 10), Call Sub, Ret]
+
+  -- Expl 4:
+  let program2 = [Push (IntVal 10), Push (IntVal 52), Call Sub, Ret]
   print $ exec program2 []
--- TEST VM
+
+  -- Expl 3:
+  let program3 = [Push (IntVal 0), Push (IntVal 42), Call Div, Ret]
+  print $ exec program3 []
+
+  -- Expl 2:
+  let program4 = [Push (IntVal 10), Push (BoolVal True), Call Add, Ret]
+  print $ exec program4 []
